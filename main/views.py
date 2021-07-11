7a5cc7ac5cd5a09f581d1c84eb8a763931090722
hyeoneedyou <hyeoneedyou@naver.com>
2021-07-12 01:04:26 +0900

    feat: Add templates

---
+++ main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def product_details(request):
+    return render(request, 'product-details.html')
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
